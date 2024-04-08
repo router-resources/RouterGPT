a4d6b1fea488196d569586bf1fc85c467e01b645
dev <home@Pranavs-Mac-mini.local>
2024-04-08 19:15:53 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+   
+      <iframe
+src="https://www.chatbase.co/chatbot-iframe/3ran-G4YkECrhJ3-_Xw5S"
+title="RouterGPT"
+className="router"
+
+></iframe>
+
+       
+   
+  )
+}
+
+export default App
